f1c245870ab352640ca16945a064dc8991ef3548
Haolan <hello@liuxi.ng>
2025-09-08 10:09:00 +0800

    feat: mock stdio paths

--- libgloss/riscv/esp/syscalls.c
+++ libgloss/riscv/esp/syscalls.c
@@ -6,21 +6,21 @@
 #include "config.h"
 #include "../../libnosys/warning.h"
 
-int
-_fstat(int file, struct stat *st)
+int _fstat(int file, struct stat *st)
 {
     if (file <= STDERR_FILENO)
     {
         st->st_mode = S_IFCHR;
-        return  0;
+        return 0;
     }
-    return  -1;
+    return -1;
 }
 
 ssize_t
 _write(int file, const char *ptr, size_t len)
 {
-    for (size_t i = 0; i < len; i++) {
+    for (size_t i = 0; i < len; i++)
+    {
         board_uart_write_char(ptr[i]);
     }
 
@@ -28,75 +28,77 @@ _write(int file, const char *ptr, size_t len)
 }
 
 struct _reent *
-__getreent (void)
+__getreent(void)
 {
     return _GLOBAL_REENT;
 }
 
 char *
-_sbrk (int nbytes)
+_sbrk(int nbytes)
 {
-  extern char _end[];
-  static char *heap_ptr;
+    extern char _end[];
+    static char *heap_ptr;
 
-  char        *base;
+    char *base;
 
-  if (!heap_ptr)
-    heap_ptr = (char *)&_end;
-  base = heap_ptr;
-  heap_ptr += nbytes;
+    if (!heap_ptr)
+        heap_ptr = (char *)&_end;
+    base = heap_ptr;
+    heap_ptr += nbytes;
 
-  return base;
+    return base;
 }
 
-int
-_kill (int sig)
+int _kill(int sig)
 {
     return -1;
 }
 
-int
-_getpid (void)
+int _getpid(void)
 {
     return -1;
 }
 
 void
-__attribute__ ((noreturn))
-_exit (int status)
+    __attribute__((noreturn))
+    _exit(int status)
 {
-    for (;;) {
+    for (;;)
+    {
         ;
     }
 }
 
-int
-_open (const char *file, int flags, int mode)
+int _open(const char *file, int flags, int mode)
 {
+    // semihosting is not support, mock some necessary paths
+    if (strcmp(file, "/dev/stdin") == 0)
+        return 0;
+    if (strcmp(file, "/dev/stdout") == 0)
+        return 1;
+    if (strcmp(file, "/dev/stderr") == 0)
+        return 2;
     return -1;
 }
 
 _ssize_t
-_read (int fd, char *buf, size_t cnt)
+_read(int fd, char *buf, size_t cnt)
 {
     return -1;
 }
 
-int
-_lseek (int fd, _off_t off, int whence)
+int _lseek(int fd, _off_t off, int whence)
 {
     return -1;
 }
 
-int
-_close (int fd)
+int _close(int fd)
 {
     return -1;
 }
 
-__attribute__((weak))
-int nanosleep(const struct timespec *duration,
-              struct timespec *rem)
+__attribute__((weak)) int nanosleep(const struct timespec *duration,
+                                    struct timespec *rem)
 {
     /* Reading csr mcycle or CSR_PCCR_MACHINE(0x7e2) is not working correctly.
      * mcycle always returns 0, and CSR_PCCR_MACHINE gives a garbage values.
